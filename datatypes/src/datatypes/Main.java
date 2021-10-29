bf1cc7164c083d1640bf5b7c6c4c4b6436c7efb8
kazimyilmaz <kazimyillmaz@gmail.com>
2021-10-29 23:32:36 +0300

    First commit.

---
+++ datatypes/src/datatypes/Main.java
@@ -0,0 +1,20 @@
+package datatypes;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		int sayi= 12; 
+		double  fiyat = 12.5 ; 
+		char karakter = 'A';   
+		String sehir ="Ankara";
+		 boolean dogruMu = false; 
+		  
+		 System.out.println(karakter);
+		
+		
+		
+
+	}
+
+}
